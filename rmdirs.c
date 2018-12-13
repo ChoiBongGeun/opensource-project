78cff9f298b96f92a5ffa482e0572a027f14aed6
passerby14 <dhy00085@gmail.com>
2018-12-14 01:30:16 +0900

    파일 삭제옵션

---
+++ rmdirs.c
@@ -0,0 +1,71 @@
+﻿#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
+int rmdirs(const char *path, int is_error_stop)
+{
+    DIR *  dir_ptr      = NULL;
+    struct dirent *file = NULL;
+    struct stat   buf;
+    char   filename[1024];
+
+    /* 목록을 읽을 디렉토리명으로 DIR *를 return 받습니다. */
+    if((dir_ptr = opendir(path)) == NULL) {
+		/* path가 디렉토리가 아니라면 삭제하고 종료합니다. */
+		return unlink(path);
+    }
+
+    /* 디렉토리의 처음부터 파일 또는 디렉토리명을 순서대로 한개씩 읽습니다. */
+    while((file = readdir(dir_ptr)) != NULL) {
+        // readdir 읽혀진 파일명 중에 현재 디렉토리를 나타네는 . 도 포함되어 있으므로 
+        // 무한 반복에 빠지지 않으려면 파일명이 . 이면 skip 해야 함
+        if(strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0) {
+             continue;
+        }
+
+        sprintf(filename, "%s/%s", path, file->d_name);
+
+        /* 파일의 속성(파일의 유형, 크기, 생성/변경 시간 등을 얻기 위하여 */
+        if(lstat(filename, &buf) == -1) {
+            continue;
+        }
+
+        if(S_ISDIR(buf.st_mode)) { // 검색된 이름의 속성이 디렉토리이면
+            /* 검색된 파일이 directory이면 재귀호출로 하위 디렉토리를 다시 검색 */
+            if(rmdirs(filename, is_error_stop) == -1 && is_error_stop) {
+                return -1;
+            }
+        } else if(S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode)) { // 일반파일 또는 symbolic link 이면
+            if(unlink(filename) == -1 && is_error_stop) {
+                return -1;
+            }
+        }
+    }
+
+    /* open된 directory 정보를 close 합니다. */
+    closedir(dir_ptr);
+    
+    return rmdir(path);
+}
+
+int main(int argc, char *argv[])
+{
+    return rmdirs("./backup", 1);
+}
+#if 0
+// Sample 1. 디렉토리 전체 삭제
+#include <string.h>
+
+/* 디렉토리 하위의 모든 파일 및 디렉토리를 삭제합니다.*/
+int main(int argc, char *argv[])
+{
+    return rmdirs("./backup", 1);
+}
+
+
+#endif
+
